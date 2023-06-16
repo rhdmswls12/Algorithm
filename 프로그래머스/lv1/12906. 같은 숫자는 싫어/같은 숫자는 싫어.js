6bc7727f6b1e9e21999694605ec6d94bdfd96b7a
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-06-16 20:26:53 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 15.59 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+ let result = []
+ for (let i=0; i<arr.length; i++) {
+  result.push(arr[i])
+  if (arr[i] == arr[i+1]){
+    result.pop(arr[i+1])
+  }
+ }
+ return result
+}
