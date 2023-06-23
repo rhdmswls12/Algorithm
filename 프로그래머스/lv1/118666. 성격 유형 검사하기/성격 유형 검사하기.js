bf0db1c99bccaef29826de3ee722cff39b6138a3
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-06-23 11:05:07 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.91 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,110 +1,25 @@
 function solution(survey, choices) {
-  
-  let point = [3, 2, 1, 0, 1, 2, 3]
-  let disagree = { 
-    'R': 0,
-    'C': 0, 
-    'J': 0,
-    'A': 0
-  }
-  let agree = {
-    'T': 0,
-    'F': 0,
-    'M': 0,
-    'N': 0
-  }
+  const types = ["RT","CF","JM","AN"]
   let result = ''
-  
-  for (let i=0; i<survey.length; i++) {
-    if(survey[i].slice(0,1) == 'R') {
-      if (choices[i] > 3) {
-        agree['T'] += point[choices[i]-1]
-      } else {
-        disagree['R'] += point[choices[i]-1]
-      }
-    } else if (survey[i].slice(0,1) == 'C') {
-      if (choices[i] > 3) {
-        agree['F'] += point[choices[i]-1]
-      } else {
-        disagree['C'] += point[choices[i]-1]
-      }
-    } else if (survey[i].slice(0,1) == 'J') {
-      if (choices[i] > 3) {
-        agree['M'] += point[choices[i]-1]
-      } else {
-        disagree['J'] += point[choices[i]-1]
-      }
-    } else if (survey[i].slice(0,1) == 'A') {
-      if (choices[i] > 3) {
-        agree['N'] += point[choices[i]-1]
-      } else {
-        disagree['A'] += point[choices[i]-1]
-      }
-    } else {
-      if (survey[i].slice(0,1) == 'T') {
-        if (choices[i] > 3) {
-          disagree['R'] += point[choices[i]-1]
-        } else {
-          agree['T'] += point[choices[i]-1]
-        }
-      } else if (survey[i].slice(0,1) == 'F') {
-        if (choices[i] > 3) {
-          disagree['C'] += point[choices[i]-1]
-        } else {
-          agree['F'] += point[choices[i]-1]
-        }
-      } else if (survey[i].slice(0,1) == 'M') {
-        if (choices[i] > 3) {
-          disagree['J'] += point[choices[i]-1]
-        } else {
-          agree['M'] += point[choices[i]-1]
-        }
-      } else if (survey[i].slice(0,1) == 'N') {
-        if (choices[i] > 3) {
-          disagree['A'] += point[choices[i]-1]
-        } else {
-          agree['N'] += point[choices[i]-1]
-        }
-      }
-    }
+  let MBTI = {}
+  survey.forEach((char) => {
+    char.split('')
+    .forEach((a) => {
+      MBTI[a] = 0
+    })
+  })
+  for (let i=0; i<choices.length; i++) {  
+    choices[i] > 4 ? MBTI[survey[i].split('')[1]] += Math.abs(4-choices[i]) 
+                   : MBTI[survey[i].split('')[0]] += Math.abs(4-choices[i]) 
   }
-
-  
-  if (agree['T'] > 0 || disagree['R'] > 0) {
-    if (agree['T'] > disagree['R']) {
-      result += 'T'
+  types.map((a, i) => {
+    if(MBTI[a.split('')[0]] > MBTI[a.split('')[1]]) {
+      result += a.split('')[0]
+    } else if (MBTI[a.split('')[0]] < MBTI[a.split('')[1]]) {
+      result += a.split('')[1]
     } else {
-      result += 'R'
+      result += types[i].split('')[0]
     }
-  } else {
-    result += 'R'
-  }
-  if (agree['F'] > 0 || disagree['C'] > 0) {
-    if (agree['F'] > disagree['C']) {
-      result += 'F'
-    } else {
-      result += 'C'
-    }
-  } else {
-    result += 'C'
-  }
-  if (agree['M'] > 0 || disagree['J'] > 0) {
-    if (agree['M'] > disagree['J']) {
-      result += 'M'
-    } else {
-      result += 'J'
-    }
-  } else {
-    result += 'J'
-  }
-  if (agree['N'] > 0 || disagree['A'] > 0) {
-    if (agree['N'] > disagree['A']) {
-      result += 'N'
-    } else {
-      result += 'A'
-    }
-  } else {
-    result += 'A'
-  }
+  })
   return result
 }
