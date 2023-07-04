152184ed226cfdb0959da8fb90cf2302748caea3
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-07-04 16:29:27 +0900

    [level 1] Title: 폰켓몬, Time: 1.95 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,7 +1,6 @@
 function solution(nums) {
   let ponketmon = {}
   let count = 0
-  let result = []
 
   nums.forEach(num => {
     ponketmon[num] = (ponketmon[num] || 0) + 1
@@ -10,7 +9,6 @@ function solution(nums) {
   const valuesOfPonketmon = Object.values(ponketmon).sort()
   valuesOfPonketmon.map((a, i) => {
     if (count < Math.floor(nums.length / 2)) {
-      result.push(a)
       count++
     }
   })
