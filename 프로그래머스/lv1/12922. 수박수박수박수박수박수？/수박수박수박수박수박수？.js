4e5987add740e3e91ea14658b7553b6799f8ad8e
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-07-15 21:16:58 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,7 @@
 function solution(n) {
   let answer = []
   for (let i=0; i<n; i++) {
-    if (i % 2) {
-      answer.push('박')
-    } else {
-      answer.push('수')
-    }
+    i % 2 ? answer.push('박') : answer.push('수')
   }
   return answer.join('')
-}
+} 
