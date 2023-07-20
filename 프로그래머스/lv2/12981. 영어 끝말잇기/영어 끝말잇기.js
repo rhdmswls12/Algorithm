bc026d68ac1e1fac706c612167078351ec4848e6
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-07-20 10:15:58 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,24 @@
+function solution(n, words) {
+  
+  let answer = []
+  let result = [0, 0]
+
+  for (let i=0; i<words.length; i++) {
+    
+    answer.push(words[i])
+
+    if (i < words.length - 1) {
+      if (answer[i].slice(-1) != words[i+1].slice(0,1) || answer.includes(words[i+1])) {
+        result[0] = (i+2) % n ? (i+2) % n : n
+        result[1] = Math.ceil((i+2) / n)
+        break
+      } 
+      if (answer.includes(words[i+1])) {
+        result[0] = (i+2) % n ? (i+2) % n : n
+        result[1] = Math.ceil((i+2) / n)
+        break
+      }
+    } 
+  }
+  return result
+}
