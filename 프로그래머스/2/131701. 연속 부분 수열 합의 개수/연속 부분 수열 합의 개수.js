7347841565b3e2181c350311d6d8f65991fc3fc0
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-12 18:50:40 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1972.58 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -7,12 +7,11 @@ function solution(elements) {
           let willAdd = elements.slice(0, i-1)
           let newArray = [...elements, ...willAdd]
           for (let j=0; j<newArray.length; j++) {
-              let split = newArray.slice(j, j+i)
-              let sum = split.reduce((acc, cur) => (acc + cur), 0)
+              let sum = newArray.slice(j, j+i).reduce((acc, cur) => (acc + cur), 0)
               set.add(sum)
           }
       }
   }
-    result = [...set].sort((a, b) => a - b)
+    result = [...set]
     return result.length
 }
