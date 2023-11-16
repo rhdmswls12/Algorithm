db72f8f16b1ffbcc407b64e127665aa78a347d1a
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-16 19:35:26 +0900

    [level 2] Title: [1차] 캐시, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -2,24 +2,22 @@ function solution(cacheSize, cities) {
     let time = 0
     let cache = []
     
-    cities.forEach((city, i) => cities[i] = cities[i].toUpperCase())
+    if (!cacheSize) return 5 * cities.length;
     
-    if (!cacheSize) return cities.length*5
-    
-    for (let i=0; i<cities.length; i++) {
-        if (cache.includes(cities[i])) {
-            time+=1
-            let idx = cache.findIndex(c => c === cities[i])
+    cities.forEach(city => {
+        city = city.toUpperCase()
+        let idx = cache.indexOf(city)
+        
+        if (idx !== -1) {
+            time += 1
             cache.splice(idx, 1)
         } else {
-            time+=5
+            time += 5
+            if (cache.length >= cacheSize) {
+                cache.shift()
+            }
         }
-        if (cache.length === cacheSize) {
-            cache.shift()
-            cache.push(cities[i])
-        } else {
-            cache.push(cities[i])
-        }
-    }
+        cache.push(city)
+    })
     return time
 }
