a05fe676f0d3d5c3b868abb3052e9134780c64f5
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-23 18:02:32 +0900

    [level 2] Title: 튜플, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,14 +1,12 @@
 function solution(s) {
-    let array = []
     let set = new Set()
-    
-    array = JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'))
-    array.sort((a, b) => a.length - b.length)
-    for (let i=0; i<array.length; i++) {
-        for (let j=0; j<array[i].length; j++) {
-            set.add(array[i][j])
+    let answer = []
+    s = JSON.parse(s.replace(/{/g,'[').replace(/}/g,']')).sort((a, b) => a.length - b.length)
+    for (let i=0; i<s.length; i++) {
+        for (let j=0; j<s[i].length; j++) {
+            set.add(s[i][j])
         }
     }
-    set = [...set]
-    return set
+    answer = [...set]
+    return answer
 }
