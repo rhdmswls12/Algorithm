a88bba5343fa9c58c14baef0c470034e79de34dd
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-11-28 17:38:19 +0900

    [level 1] Title: 최소직사각형, Time: 8.24 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491#qna) 
 
 ### 성능 요약
 
-메모리: 42.1 MB, 시간: 16.01 ms
+메모리: 41.9 MB, 시간: 8.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 16:28:1
+2023년 11월 2일 17:38:18
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,18 +1,12 @@
 function solution(sizes) {
-  let newSizes = []
-  let width = []
-  let height = []
-  
-  for(let i=0; i<sizes.length; i++) {
-    let cell = sizes[i].sort((a, b) => b - a)
-    newSizes.push(cell)
-  }
-  for (let i=0; i<newSizes.length; i++) {
-    width.push(newSizes[i][0])
-    height.push(newSizes[i][1])
-  }
-  width.sort((a, b) => b - a)
-  height.sort((a, b) => b - a)
-
-  return width[0] * height[0]
+    sizes.map(size => size.sort((a, b) => b - a))
+    let width = []
+    let height = []
+    for (let i=0; i<sizes.length; i++) {
+        width.push(sizes[i][0])
+        height.push(sizes[i][1])
+    }
+    let maxWidth = Math.max(...width)
+    let maxHeight = Math.max(...height)
+    return maxWidth * maxHeight
 }
