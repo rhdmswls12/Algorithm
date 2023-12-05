811a3a2f085707acd244bec6513ea35414f6243a
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2023-12-05 13:45:33 +0900

    [Bronze V] Title: A+B, Time: 116 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		int A;
+		int B;
+		int sum;
+		Scanner input=new Scanner(System.in);
+		A=input.nextInt();
+		B=input.nextInt();
+		sum=A+B;
+		System.out.println(sum);
+	}
+}
