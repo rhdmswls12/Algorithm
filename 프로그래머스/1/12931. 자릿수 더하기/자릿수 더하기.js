cc0a24170eb9671336fb974ef3d51170d97b2c48
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-05-11 21:55:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,8 +1,7 @@
 function solution(n) {
-    let answer = 0
-    let s = n.toString().split('')
-    s.forEach(str => {
-        answer += parseInt(str)
-    })
+    let s = (n + '').split('')
+    let answer = s.reduce((acc, cur) => {
+        return acc + parseInt(cur)
+    },0)
     return answer
 }
