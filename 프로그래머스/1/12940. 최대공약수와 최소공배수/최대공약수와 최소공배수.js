46043798645261109eed10f080113ec156b9e8aa
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-05-21 17:46:48 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 9.33 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,7 +1,6 @@
 function solution(n, m) {
     let gcd = Math.max(...getDivisors(n,m))
-    let lcm = Math.min(...getCommonMultiple(n,m))
-    return [gcd, lcm]
+    return [gcd, n*m / gcd]
 }
 function getDivisors(num1, num2) {
     let arr = []
@@ -9,13 +8,6 @@ function getDivisors(num1, num2) {
         for (let j=1; j<=num2; j++) {
             if (num1%i === 0 && num2%j === 0 && i===j ) arr.push(i)
         }
-    }
-    return arr
-}
-function getCommonMultiple(num1, num2) {
-    let arr = []
-    for (let i=1; i<=num1 * num2; i++) {
-        if (i%num1 === 0 && i%num2 === 0) arr.push(i)
     }
     return arr
 }
