2fb186b6fff36daa9372fd567fb90eb41ea0ac62
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-05-21 18:34:25 +0900

    [level 1] Title: 예산, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,11 +1,11 @@
 function solution(d, budget) {
+    d.sort((a, b) => a - b)
     while (d.length) {
-        let max = Math.max(...d)
         let sum = d.reduce((acc, cur) => acc + cur, 0)
         if (sum <= budget) {
             return d.length
         } else {
-            d.splice(d.indexOf(max),1)
+            d.splice(-1)
         }
     }
     return d.length
