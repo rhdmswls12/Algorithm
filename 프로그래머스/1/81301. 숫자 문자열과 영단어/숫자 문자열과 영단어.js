2a42383fcb125bc499fe35494c77fb2d7640adf8
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-05-22 15:18:42 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,26 +1,22 @@
 function solution(s) {
+    let str = s.split('')
+    let sum = ''
     let answer = ''
-    let numbers = {
-        'zero': '0',
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9'
+    let numbers1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    let numbers2 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
+    for (let i=0; i<=str.length; i++) {
+        let number1 = numbers1.findIndex((num) => num === sum)
+        let number2 = numbers2.findIndex((num) => num === sum)
+        if (number1 !== -1) {
+            answer += number1
+            sum = ''
+        } else if (number2 !== -1) {
+            answer += number2
+            sum = ''
+        } 
+        sum += str[i]
+        
     }
-    let str = ''
-    for (let i=0; i<=s.length; i++) {
-        for (const key in numbers) {
-            if (str === key || str === numbers[key]) {
-                answer += numbers[key]
-                str = ''
-            }
-        }
-        str += s[i]
-    }
-    return Number(answer)
+    return parseInt(answer)
 }
