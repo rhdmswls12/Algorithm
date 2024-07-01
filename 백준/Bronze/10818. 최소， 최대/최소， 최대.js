ecb58fbb77dd42b420ed926044bf0dca4445493f
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-07-01 21:18:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 392 ms, Memory: 93612 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" ").map(Number));
+
+const array = input[1];
+console.log(Math.min(...array) + " " + Math.max(...array));
