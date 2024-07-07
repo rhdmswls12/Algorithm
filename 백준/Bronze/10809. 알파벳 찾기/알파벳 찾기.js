c0633ea4920d464d0542256a6ec7f723f09c5a60
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-07-07 12:30:25 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 164 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -2,16 +2,8 @@ const input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
   .split("");
-let alphabet = {};
-for (let i = 97; i <= 122; i++) {
-  alphabet[`${String.fromCharCode(i)}`] = -1;
+let arr = [];
+for (let i = "a".charCodeAt(); i <= "z".charCodeAt(); i++) {
+  arr.push(input.indexOf(String.fromCharCode(i)));
 }
-for (let i = 0; i < input.length; i++) {
-  for (let key in alphabet) {
-    if (alphabet[key] !== -1) continue;
-    if (input[i] == key) {
-      alphabet[key] = i;
-    }
-  }
-}
-console.log(Object.values(alphabet).join(" "));
+console.log(arr.join(" "));
