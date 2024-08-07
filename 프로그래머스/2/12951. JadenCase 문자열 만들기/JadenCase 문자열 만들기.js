ca06e54c06e922adab7f439933dbad772456d566
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-08-07 19:21:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.16 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,10 +1,11 @@
 function solution(s) {
-  let answer = []
-  s = s.toLowerCase().split('')
-    
-  if (!Number(s[0])) s[0] = s[0].toUpperCase()
-  for (let i=1; i<s.length; i++) {
-      if (s[i-1] === ' ' && s[i] !== ' ') s[i] = s[i].toUpperCase()
-  }
-  return s.join('')
+    let answer = ""
+    for (let i=0; i<s.length; i++) {
+        if (i==0 || s[i-1] == " ") {
+           answer += s[i].toUpperCase();
+         } else {
+            answer += s[i].toLowerCase();
+         }
+    }
+    return answer
 }
