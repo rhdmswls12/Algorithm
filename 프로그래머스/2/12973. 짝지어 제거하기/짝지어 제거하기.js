38c56bb9dc725973fb7cd15363c72c601ba1efeb
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-08-13 19:14:23 +0900

    [level 2] Title: 짝지어 제거하기, Time: 77.27 ms, Memory: 43.5 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,12 +1,11 @@
 function solution(s) {
-  let stack = []
-  
-  for (let i=0; i<s.length; i++) {
-      if (stack[stack.length-1] === s[i]) {
-          stack.pop()
-      } else {
-          stack.push(s[i])
-      }
-  }
-  return stack.length ? 0 : 1
+    let stack = [];
+    for (let i=0; i<s.length; i++) {
+        if (stack[stack.length-1] == s[i]) {
+            stack.pop();
+        } else {
+            stack.push(s[i])
+        }
+    }
+    return stack.length ? 0 : 1;
 }
