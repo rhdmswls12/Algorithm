924479c538bb89500f4cea3fdc7f5dcadc3a836b
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-08-14 16:32:51 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 12368 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -3,51 +3,26 @@ const input = require("fs")
   .toString()
   .trim()
   .split("\n");
-let [N, M] = input[0].split(" ");
-N = Number(N);
-M = Number(M);
-let arr = input.slice(1).map((element) => element.split("").map(Number)); // 미로
-
-let dist = Array.from({ length: N }, () => new Array(M).fill(0));
-dist[0][0] = 1;
-
-let dx = [1, -1, 0, 0];
-let dy = [0, 0, -1, 1];
-let q = [[0, 0]];
+const [N, M] = input.shift().split(" ").map(Number);
+const miro = input.map((el) => el.split("").map(Number));
+const visited = Array.from({ length: N }, () => new Array(M).fill(0));
+const dx = [1, 0, -1, 0];
+const dy = [0, 1, 0, -1];
+const q = [[0, 0]];
+visited[0][0] = 1;
 
 while (q.length) {
-  let [curX, curY] = q.shift();
-
-  // let a = q.shift();
-  // let curX = a[0];
-  // let curY = a[1];
-
-  // console.log(curX, curY);
-
-  // let move = a[2];
+  let [currX, currY] = q.shift();
 
   for (let i = 0; i < 4; i++) {
-    let [nextX, nextY] = [curX + dx[i], curY + dy[i]];
+    let [nextX, nextY] = [currX + dx[i], currY + dy[i]];
 
-    // curX = curX + dx[i];
-    // curY = curY + dy[i];
-
-    // console.log(nextX, nextY);
-
-    // console.log();
-    // console.log(curX, curY);
-
-    if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
-    if (!dist[nextX][nextY] && arr[nextX][nextY]) {
-      dist[nextX][nextY] = dist[curX][curY] + 1;
-      q.push([nextX, nextY]);
+    if (nextX > -1 && nextX < N && nextY > -1 && nextY < M) {
+      if (miro[nextX][nextY] && !visited[nextX][nextY]) {
+        visited[nextX][nextY] = visited[currX][currY] + 1;
+        q.push([nextX, nextY]);
+      }
     }
-
-    // if (curX == M && curY == N) return move;
-    // if (arr[curX][curY] == 1 && (curX < 0 || curX > M || curY < 0 || curY > N))
-    // arr[curX][curY] = 0;
-    // q.push([curX, curY, move + 1]);
   }
 }
-
-console.log(dist[N - 1][M - 1]);
+console.log(visited[N - 1][M - 1]);
