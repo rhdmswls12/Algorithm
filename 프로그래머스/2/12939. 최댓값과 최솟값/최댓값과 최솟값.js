d30129b1d36b068983fae15429fe93b123f81e43
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-11-24 19:27:54 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,4 @@
 function solution(s) {
-    const mixVal = Math.min(...s.split(" "))
-    const maxVal = Math.max(...s.split(" "))
-    return mixVal + " " + maxVal;
+    let nums = s.split(" ").map(e => Number(e));
+    return Math.min(...nums) + " " + Math.max(...nums);
 }
