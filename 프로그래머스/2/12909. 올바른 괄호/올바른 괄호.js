1b3ee0d7799e0c96f6cecddd977ecfbfc5dfcba7
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2024-12-01 23:48:37 +0900

    [level 2] Title: 올바른 괄호, Time: 26.40 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,12 +1,23 @@
 function solution(s){
-    let stack = [];
+//     let stack = [];
     
-    for (let i=0; i<s.length; i++) {
-        if (stack[stack.length-1] === '(' && s[i] === ')') {
-            stack.pop();
+//     for (let i=0; i<s.length; i++) {
+//         if (stack[stack.length-1] === '(' && s[i] === ')') {
+//             stack.pop();
+//         } else {
+//             stack.push(s[i])
+//         }
+//     }
+//     return stack.length ? false : true;
+    let answer = 0;
+    
+    for (let p of s) {
+        if (p === '(') {
+            answer += 1;
         } else {
-            stack.push(s[i])
+            answer -= 1;
         }
+        if (answer < 0) return false;
     }
-    return stack.length ? false : true;
+    return answer === 0 ? true : false;
 }
