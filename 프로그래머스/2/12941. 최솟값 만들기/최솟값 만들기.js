107b1c62d37625782dc1df2f224cff80f91958ea
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-01-15 15:54:57 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.80 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,10 +1,12 @@
 function solution(A,B){
-    let answer = 0;
     A = A.sort((a, b) => a - b);
     B = B.sort((a, b) => b - a);
+    
+    let answer = 0;
     
     for (let i=0; i<A.length; i++) {
         answer += A[i] * B[i];
     }
+    
     return answer;
 }
