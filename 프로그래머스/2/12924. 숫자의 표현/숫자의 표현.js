03e397a564dc0d6c2621882b2735da05be24935a
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-01-22 16:08:19 +0900

    [level 2] Title: 숫자의 표현, Time: 0.25 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,11 +1,9 @@
-// 약수 중에서 홀수의 개수
 function solution(n) {
     let answer = 0;
     
     for (let i=1; i<=n; i++) {
-        if (n % i === 0 & i%2) {
-            answer += 1;
-        }
+        if (n%i === 0 && i%2 === 1) answer++;
     }
+    
     return answer;
 }
