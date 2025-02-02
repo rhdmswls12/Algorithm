6e5c6846c823de56a04df019c8030b0c3d6ab4ea
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-02-02 22:39:52 +0900

    [level 2] Title: 구명보트, Time: 53.04 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,15 +1,17 @@
 function solution(people, limit) {
-   people = people.sort((a, b) => a - b)
-   let count = 0
-   
-   while(people.length) {
-       if (people[0]+people[people.length-1] <= limit) {
-           people.shift()
-           people.pop()
-       } else {
-           people.pop()
-       }
-       count++
-   }
-   return count
+    let count = 0;
+    people.sort((a, b) => a - b);
+    
+    while (people.length) {
+        for (let i=0; i<people.length; i++) {
+            if (people[0] + people[people.length-1] <= limit) {
+                people.shift();
+                people.pop();
+            } else {
+                people.pop();
+            }
+            count++;
+        }        
+    }
+    return count;
 }
