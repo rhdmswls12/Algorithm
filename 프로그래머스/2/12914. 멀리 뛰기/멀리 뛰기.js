b390eada99651908f5bf701a331e5c7dcef4e3fa
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-02-05 22:58:24 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,7 +1,11 @@
 function solution(n) {
-  let f = [0, 1, 2]
-  for (let i=3; i<=n; i++) {
-      f[i] = (f[i-2] + f[i-1]) % 1234567
-  }
-  return f[n]
+    let arr = [];
+    arr[0] = 1;
+    arr[1] = 2;
+    
+    for (let i=2; i<n; i++) {
+        arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+    }
+    
+    return arr[n-1];
 }
