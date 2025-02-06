fda1b2bea1fd060d9a0ca77dfd64d2135defd24b
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-02-06 17:10:14 +0900

    [level 2] Title: N개의 최소공배수, Time: 3846.01 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,19 +1,26 @@
+// a,b의 최소공배수 = a*b / a,b의 최대공약수
 function solution(arr) {
-  let stack = [...arr]
-  
-  while(stack.length !== 1) {
-      let result = getLcd(stack.pop(), stack.pop())
-      stack.push(result)
-  }
-  return stack[0]
-}
-const getLcd = (num1, num2) => {
-    let lcd = 1
-    while(true) {
-        if (lcd % num1 === 0 && lcd % num2 === 0) {
-            break
-        }
-        lcd++
+    let answer = lcm(arr[0], arr[1]);
+    
+    for (let i=2; i<arr.length; i++) {
+        answer = lcm(answer, arr[i]);
     }
-    return lcd
+    return answer;
+}
+
+//최소공배수
+function lcm(a, b) {
+    return (a*b)/gcd(a, b);
+}
+//최대공약수
+function gcd(a, b) {
+    let multi = a*b;
+    let arr = [];
+    
+    for (let i=1; i<=multi; i++) {
+        if (multi % i === 0) arr.push(i);
+    }
+    for (let i=arr.length-1; i>=0; i--) {
+        if ((a % arr[i] === 0) && (b % arr[i] === 0)) return arr[i];
+    }
 }
