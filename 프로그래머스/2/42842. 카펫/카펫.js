29424f71531a6610d4918b220866b853daeb7a53
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-02-06 15:38:01 +0900

    [level 2] Title: 카펫, Time: 10.91 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,16 +1,23 @@
+// (brown + yellow)의 약수 쌍 (w, h)을 구한다.
+// 각 (w, h) 쌍에 대해 w >= h인지 확인한다.
+// (w - 2) * (h - 2) == yellow를 만족하는지 체크한다.
 function solution(brown, yellow) {
-    let array = []
-    
-    for (let i=1; i<=yellow; i++) {
-        if (yellow % i === 0 && yellow/i >= i) {
-            array.push([yellow/i, i])
+    let answer = [];
+    let nums = [];
+    for (let i=1; i<=brown+yellow; i++) {
+        if ((brown+yellow) % i === 0) {
+            nums.push(i);
         }
     }
-    for (let i=0; i<array.length; i++) {
-        if ((array[i][0]+2)*(array[i][1]+2) - yellow === brown) {
-            array[i][0] = array[i][0]+2
-            array[i][1] = array[i][1]+2
-            return array[i]
+    for (let i=nums.length-1; i>0; i--) {
+        for (let j=0; j<nums.length; j++) {
+            let w = nums[i];
+            let h = nums[j];
+            if (w>=h && (w-2)*(h-2)===yellow) {
+                answer = [w, h];
+            }
         }
     }
+    console.log(answer);
+    return answer;
 }
