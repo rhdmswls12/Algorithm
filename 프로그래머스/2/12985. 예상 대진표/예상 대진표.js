cf2e6123322da52193501041a27b3a3b7d7edd35
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-02-11 21:05:57 +0900

    [level 2] Title: 예상 대진표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,9 +1,9 @@
-function solution(n,a,b) {
-  let count = 0
-  while(a !== b) {
-      a = Math.round(a / 2)
-      b = Math.round(b / 2)
-      count++
-  }
-  return count
+function solution(n,a,b){
+    let answer = 0;
+    while (a !== b) {
+        a = Math.round(a/2);
+        b = Math.round(b/2);
+        answer++;
+    }
+    return answer;
 }
