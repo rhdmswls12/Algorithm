578cfcd6ce30ce5d8889839fbf38dd4dd72abe55
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-02-12 17:19:12 +0900

    [level 2] Title: 괄호 회전하기, Time: 52.88 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -10,21 +10,16 @@ function solution(s) {
 }
 function check(arr) {
     const points = {
-        '[': -1,
-        ']': 1,
-        '(': -2,
-        ')': 2,
-        '{': -3,
-        '}': 3
+        '[': ']',
+        '(': ')',
+        '{': '}',
     }
     let stack = [];
     
     for (let i=0; i<arr.length; i++) {
         let last = stack[stack.length-1];
         
-        if (points[last] > 0) return false;
-        
-        if (points[last] + points[arr[i]] === 0) {
+        if (points[last] === arr[i]) {
             stack.pop();
         } else {
             stack.push(arr[i]);
