9115e3bd1c581b2073625c54c5aad6f614678d5c
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-02-19 18:03:39 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 9.13 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#qna) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.45 ms
+메모리: 36.9 MB, 시간: 9.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 21:34:44
+2025년 02월 19일 18:03:36
 
 ### 문제 설명
 
--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,30 +1,44 @@
 function solution(str1, str2) {
-    let intersect = 0
-    let union = 0
-    const a = makeSet(str1)
-    const b = makeSet(str2)
+    str1 = str1.toUpperCase().split('');
+    str2 = str2.toUpperCase().split('');
     
-    if (!a.length && !b.length) return 65536
+    const arr1 = makeArr(str1);
+    const arr2 = makeArr(str2);
     
-    let set = new Set([...a, ...b])
-    set.forEach(s => {
-        let has1 = a.filter(item => item === s).length
-        let has2 = b.filter(item => item === s).length
-        intersect += Math.min(has1, has2)
-        union += Math.max(has1, has2)
-    })
+    if (!arr1.length && !arr2.length) return 65536;
     
-    return Math.trunc((intersect/union)*65536)
-}
-const makeSet = (str) => {
-    const regex = /^[a-zA-Z]+$/
-    str = str.split('')
-    let array = []
-    for (let i=0; i<str.length; i++) {
-        if (i<str.length-1) {
-            let sliced = str.slice(i, i+2).join('')
-            if (regex.test(sliced)) array.push(sliced.toUpperCase())
+    let same = [];
+    let visited = Array(arr2.length).fill(false);
+    
+    for (let i=0; i<arr1.length; i++) {
+        for (let j=0; j<arr2.length; j++) {
+            if (!visited[j] && arr1[i] === arr2[j]) {
+                same.push(arr1[i]);
+                visited[j] = true;
+                break;
+            }
         }
     }
-    return array
+    console.log('same', same)
+    const result1 = same.length;
+    console.log(arr1, arr2)
+    const result2 = arr1.length + arr2.length - result1;
+    
+    return Math.floor((result1/result2)*65536);
+} 
+function makeArr(arr) {
+    let result = [];
+    for (let i=0; i<arr.length; i++) {
+        let sum = '';
+        
+        if (arr[i+1] 
+            && arr[i].charCodeAt() >=65 
+            && arr[i].charCodeAt() <=90 
+            && arr[i+1].charCodeAt() >=65
+            && arr[i+1].charCodeAt() <=90) {
+            sum += arr[i]+arr[i+1];
+            result.push(sum);
+        }
+    }
+    return result;
 }
