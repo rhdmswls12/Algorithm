090c72e754f4107ca59fdced92a7526c26c1591c
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-03-18 14:14:52 +0900

    [level 2] Title: 전화번호 목록, Time: 133.08 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,10 +1,10 @@
 function solution(phone_book) {
     phone_book.sort();
     
-    for (let i=0; i<phone_book.length; i++) {
+    for (let i=0; i<phone_book.length-1; i++) {
         let cur = phone_book[i];
         
-        if (i+1 < phone_book.length && phone_book[i+1].startsWith(cur)) return false;
+        if (phone_book[i+1].startsWith(cur)) return false;
     }
     return true;
 }
