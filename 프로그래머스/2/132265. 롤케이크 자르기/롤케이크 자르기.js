03a78a9c7fa4b08150f8e685fe3dcae7795ec1a8
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-03-24 15:22:38 +0900

    [level 2] Title: 롤케이크 자르기, Time: 109.01 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,35 +1,22 @@
-// function solution(topping) {
-//     let answer = 0;
-    
-//     for (let i=0; i<topping.length-1; i++) {
-//         const slice1 = new Set(topping.slice(0, i+1));
-//         const slice2 = new Set(topping.slice(i+1));
-        
-//         if (slice1.size === slice2.size) answer++;
-//     }
-//     return answer;
-// }
-
 function solution(topping) {
     let answer = 0;
     let left_set = new Set();
-    let right_set = new Set(topping);
-    let right_count = {};
     
-    topping.forEach(el => {
-        right_count[el] = right_count[el] ? right_count[el]+1 : 1;
-    })
+    let right_count = topping.reduce((acc, cur) => {
+        acc[cur] = (acc[cur] || 0) + 1;
+        return acc;
+    }, {});
+    
+    let right_unique = Object.keys(right_count).length;
     
     for (let i=0; i<topping.length-1; i++) {
         left_set.add(topping[i]);
-        if (right_count[topping[i]] > 1) {
-            right_count[topping[i]]--;
-        } else {
-            right_set.delete(topping[i]);
-            right_count[topping[i]]--;
-        }
+        right_count[topping[i]]--;
+        
+        if (!right_count[topping[i]]) right_unique--;
 
-        if (left_set.size === right_set.size) answer++;
+        if (left_set.size === right_unique) answer++;
     }
+    
     return answer;
 }
