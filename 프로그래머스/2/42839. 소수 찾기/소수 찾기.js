640276497f67fc228f8e4e190911e26e4ea4873f
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-11-18 16:23:57 +0900

    [level 2] Title: 소수 찾기, Time: 7.49 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,32 +1,41 @@
 function solution(numbers) {
     let answer = 0;
-    let set = new Set();
+    const nums = permute(numbers);
     
-    function permutation(arr, fixed) { 
-        if (fixed.length) set.add(Number(fixed));
-
-        for (let i=0; i<arr.length; i++) {
-            let newArr = [...arr];
-            newArr.splice(i, 1); 
-            permutation(newArr, fixed+arr[i]); 
-        }
+    for (const num of nums) {
+        if (isPrime(Number(num))) answer++;
     }
+    
+    function permute(str) {
+      const result = new Set();
+      const visited = Array(str.length).fill(false);
+
+      function backtrack(path) {
+        if (path.length && path[0] !== '0') {
+          result.add([...path].join(''));
+        }
+
+        for (let i = 0; i < str.length; i++) {
+          if (visited[i]) continue;
+          visited[i] = true;
+          path.push(str[i]);
+          backtrack(path);
+          path.pop();
+          visited[i] = false;
+        }
+      }
+
+      backtrack([]);
+      return [...result];
+    }
+        
     function isPrime(num) {
         if (num < 2) return false;
         
-        for (let i=2; i<Math.floor(Math.sqrt(num)+1); i++) {
-            if (num % i === 0) {
-                return false;
-            }
+        for (let i=2; i<=Math.floor(Math.sqrt(num)); i++) {
+            if (num % i === 0) return false;
         }
         return true;
-    }
-    permutation(numbers.split(''), "");
-    
-    const numArray = [...set];
-    
-    for (let i=0; i<numArray.length; i++) {
-        if (isPrime(numArray[i])) answer++;
     }
     
     return answer;
