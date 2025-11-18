83a1f4dcce17e9a5739f2ddeed6e44c1ad7220d4
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-11-18 21:10:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.74 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,18 @@
 function solution(participant, completion) {
-  let myMap = new Map()
-  let answer = ''
-
-  for (let p of participant) {
-    if (!myMap.get(p)) {
-      myMap.set(p, 1)
-    } else {
-      myMap.set(p, myMap.get(p) + 1)
+    let answer = '';
+    const participantMap = new Map();
+    
+    for (const p of participant) {
+        participantMap.set(p, (participantMap.get(p) || 0 ) + 1);
     }
-  }
-  for (let c of completion) {
-    if (myMap.get(c)) {
-      myMap.set(c, myMap.get(c) - 1)
+    
+    for (const c of completion) {
+        participantMap.set(c, (participantMap.get(c) - 1));
     }
-  }
-  for (let [key, value] of myMap) {
-    if (value !== 0) {
-      answer = key
-    } 
-  }
-  return answer
+    
+    for (const [key, value] of participantMap) {
+        if (value === 1) answer = key;
+    }
+    
+    return answer;
 }
