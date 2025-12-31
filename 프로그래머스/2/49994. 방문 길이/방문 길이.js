d0dff7126dab2259e3bd9ddbc1de8b392436f8d9
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2025-12-31 14:28:20 +0900

    [level 2] Title: 방문 길이, Time: 0.96 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -2,26 +2,24 @@ function solution(dirs) {
     const move = {
         U: [0, 1],
         D: [0, -1],
-        L: [-1, 0],
         R: [1, 0],
+        L: [-1, 0]
     };
-    let x = 0, y = 0;
-    let visited = new Set();
+    const visited = new Set();
+    let [x, y] = [0, 0];
     
-    for (let dir of dirs) {
+    for (const dir of dirs) {
         const [dx, dy] = move[dir];
-        
-        const nx = x + dx;
-        const ny = y + dy;
-        
-        if (nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
-        
-        visited.add(`${x} ${y} ${nx} ${ny}`);
-        visited.add(`${nx} ${ny} ${x} ${y}`);
-        
-        x = nx;
-        y = ny;
-        
+        let nx = x + dx;
+        let ny = y + dy;
+
+        if (nx >= -5 && nx <=5 && ny >= -5 && ny <= 5) {
+            visited.add(`[${[x]}, ${y}] [${nx}, ${ny}]`);
+            visited.add(`[${nx}, ${ny}] [${x}, ${y}]`);
+            
+            x = nx;
+            y = ny;
+        }
     }
-    return visited.size / 2
+    return visited.size / 2;
 }
