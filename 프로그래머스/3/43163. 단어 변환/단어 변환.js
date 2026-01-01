d624a07e663b7a1e6755f85a0865bede6885f594
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2026-01-01 15:34:03 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,39 +1,32 @@
 function solution(begin, target, words) {
-    let answer = 0;
-    let visited = new Set();
+    const queue = [[begin, 0]];
+    const visited = new Set();
+    let idx = 0;
+    visited.add(begin);
     
     if (!words.includes(target)) return 0;
-
-    const queue = [begin];
-
-    const compare = (word1, word2) => {
-        let count = 0;
-        
-        for (let i=0; i<word1.length; i++) {
-            if (word1[i] !== word2[i]) count++;
-        }
-        
-        return count === 1 ? true : false;
-    }
     
-    while (queue.length) {
-        const levelSize = queue.length;
+    while (idx < queue.length) {
+        const [cur, count] = queue[idx++];
         
-        for (let i=0; i<levelSize; i++) {
-            const cur = queue.shift();
-            
-            if (cur === target) return answer;
+        if (cur === target) return count;
         
-            for (const next of words) {
-                if (!visited.has(next) && compare(cur, next)) {
-                    visited.add(next);
-                    queue.push(next);
-                }
+        for (const next of words) {
+            if (!visited.has(next) && isPossible(cur, next)) {
+                visited.add(next);
+                queue.push([next, count + 1]);
             }
         }
-        
-        answer++;
+    }
+}
+function isPossible(word1, word2) {
+    let count = 0;
+    
+    for (let i=0; i<word1.length; i++) {
+        if (word1[i] !== word2[i]) {
+            count++;
+        }
     }
     
-    return answer;
+    return count === 1;
 }
