8f79089849c1fc5f0fac144065639671ad8147a2
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2026-01-03 18:54:03 +0900

    [level 2] Title: 오픈채팅방, Time: 343.36 ms, Memory: 92.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,26 @@
+function solution(record) {
+    const result = [];
+    const enter = `님이 들어왔습니다.`
+    const leave = `님이 나갔습니다.`
+    
+    const map = new Map()
+    
+    for (const r of record) {
+        const [keyword, id, nickname] = r.split(' ');
+        
+        if (keyword === 'Enter' || keyword === 'Change') {
+            map.set((id || ''), nickname);   
+        }
+    }
+    
+    for (const r of record) {
+        const [keyword, id, nickname] = r.split(' ');
+    
+        if (keyword === 'Enter') {
+            result.push(`${map.get(id)}` + `${enter}`);
+        } else if (keyword === 'Leave') {
+            result.push(`${map.get(id)}` + `${leave}`);
+        } 
+    }
+    return result;
+}
