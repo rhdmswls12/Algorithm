40483cccf52462a0759bf2ca85a98a0700b3423a
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2026-01-04 22:53:18 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
-SELECT I.ANIMAL_ID, I.NAME
-FROM ANIMAL_INS I JOIN ANIMAL_OUTS O
-ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE I.DATETIME > O.DATETIME
-ORDER BY I.DATETIME
+SELECT i.animal_id, i.name
+from animal_ins i join animal_outs o
+on i.animal_id = o.animal_id
+where i.datetime > o.datetime
+order by i.datetime;
