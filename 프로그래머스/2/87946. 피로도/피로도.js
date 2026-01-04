1717cdbeeffd2f5429462b251756a33fbc609b24
eunjin <71330240+rhdmswls12@users.noreply.github.com>
2026-01-04 13:59:55 +0900

    [level 2] Title: 피로도, Time: 8.44 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,23 +1,23 @@
 function solution(k, dungeons) {
-  let maxCount = 0;
-  const visited = Array(dungeons.length).fill(false);
-
-  function dfs(fatigue, count) {
-
-    maxCount = Math.max(maxCount, count);
-
-    for (let i = 0; i < dungeons.length; i++) {
-      const [need, cost] = dungeons[i];
-
-      if (visited[i]) continue;
-      if (fatigue < need) continue;
-
-      visited[i] = true;
-      dfs(fatigue - cost, count + 1);
-      visited[i] = false;
+    const visited = Array(dungeons.length).fill(false);
+    let maxCount = 0;
+    
+    function dfs(k, count) {
+        maxCount = Math.max(maxCount, count);
+        
+        for (let i=0; i<dungeons.length; i++) {
+            const [need, consume] = dungeons[i];
+            
+            if (k < need) continue;
+            
+            if (!visited[i])  {
+                visited[i] = true;
+                dfs(k-consume, count+1);
+                visited[i] = false;
+            }
+        }
     }
-  }
-
-  dfs(k, 0);
-  return maxCount;
+    
+    dfs(k, 0);
+    return maxCount;
 }
